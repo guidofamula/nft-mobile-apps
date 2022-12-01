273e64eb29ea04af567504603c89c36a871cec26
Guido Famula <guidofamula29@gmail.com>
2022-12-02 00:16:19 +0700

    first commit

---
+++ components/DetailsBid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const DetailsBid = () => {
+	return (
+		<View>
+			<Text>DetailsBid</Text>
+		</View>
+	);
+};
+
+export default DetailsBid;
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { Subinfo } from "./Subinfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+	HomeHeader,
+	NFTCard,
+	FocusedStatusBar,
+	CircleButton,
+	RectButton,
+	DetailsDesc,
+	DetailsBid,
+	Subinfo,
+};
